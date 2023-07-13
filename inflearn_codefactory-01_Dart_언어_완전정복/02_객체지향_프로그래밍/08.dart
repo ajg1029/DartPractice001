61ad6ddb9ae6ff7a874849e6397cccac5f398185
ajg1029 <urban.seonbi.deon@gmail.com>
2023-07-13 22:45:28 +0900

    ㅔ?

--- inflearn_codefactory-01_Dart_언어_완전정복/02_객체지향_프로그래밍/08.dart
+++ inflearn_codefactory-01_Dart_언어_완전정복/02_객체지향_프로그래밍/08.dart
@@ -1,4 +1,18 @@
 void main() {
+  Idol apink = Idol(name: '에이핑크', membersCount: 5);
+  apink.sayName();
+  apink.sayMembersCount();
+
+  BoyGroup bts = BoyGroup('방탄소년단', 7);
+
+  bts.sayName();
+  bts.sayMembersCount();
+  bts.sayBoy();
+
+  print(apink is Idol);
+  print(apink is BoyGroup);
+  print(bts is Idol);
+  print(bts is BoyGroup);
   
 
 }
@@ -13,10 +27,25 @@ class Idol {
   });
 
   void sayName() {
-    print('저는 ${this.name}입니다.');
+    print('저희는 ${this.name}입니다.');
   }
 
   void sayMembersCount() {
     print('${this.name}은 ${this.membersCount}명의 멤버가 있습니다.');
   }
+}
+
+class BoyGroup extends Idol{
+  BoyGroup(
+    String name,
+    int membersCount
+  ) : super(
+    name: name,
+    membersCount: membersCount,
+  );
+
+  void sayBoy(){
+    print('남자 아이돌');
+  }
+
 }
